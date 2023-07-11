d1a1ab5aa5409a79156f1305c1f29897680d36fc
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-11 11:30:30 +0100

    protected details page

--- client/app/details/page.tsx
+++ client/app/details/page.tsx
@@ -1,12 +1,20 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import styles from "../page.module.css";
 import DetailsPage from "./DetailsPage";
 
-const Details = () => {
+const Details = async () => {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) {
+    redirect("/login");
+  }
   return (
     <main className={styles.main}>
       <DetailsPage />
     </main>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
