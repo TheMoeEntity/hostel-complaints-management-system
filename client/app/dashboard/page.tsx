85fa2ed1e354e9434975a75bbcc3734cf7b14ea3
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-16 12:22:58 +0100

    protected dashboard page

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,8 +1,16 @@
 import DashboardPage from "@/components/dashboard";
 import styles from "../page.module.css";
 import { Helpers } from "@/Helpers/Types";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) {
+    redirect("/login");
+  }
   const dashCount =
     (await Helpers.fetchData(
       "https://hostelcomplaintsmanagementsystem.onrender.com/api/dashboard/dashboard-count/"
